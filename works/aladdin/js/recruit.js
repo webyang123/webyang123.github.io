600b15856dfd304f0f8625e6573101f4d8c74eff
xiangyang819 <lanou3g@lanou3gdeMac-mini-4.local>
2016-08-07 19:43:24 +0800

    5
    5

--- works/aladdin/js/recruit.js
+++
@@ -1,6 +0,0 @@
-$(".recruitleft li").on("click",function(){
-    $(".recruitleft li").removeClass("active");
-    $("this").addClass("active");
-    $(".recruitright .section").hide();
-    $(".recruitright .section").eq($(this).index()).show();
-})
--- works/pkey/news/js/gonggao.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".tab .tabtitle a").on("click",function(){
-        $(".tab .tabtitle a").removeClass("on")
-        $(this).addClass("on");
-        $(".tabbox").hide();
-        $(".tabbox").eq($(this).index()).show();
-    })
-})
